5a579b66724e8ee2c782495bd804410de78063e6
ovindu <ovindunambukara@gmail.com>
2021-07-20 20:56:18 +0530

    change navigation dropdown categories

--- src/components/bottom-nav/BottomNav.tsx
+++ src/components/bottom-nav/BottomNav.tsx
@@ -7,12 +7,21 @@ const BottomNav: React.FC = () => {
       <Container fluid={true}>
         <Navbar className="bottom-nav-bar px-0 py-3 border-bottom" expand="sm">
           <NavDropdown title="Categories" id="basic-nav-dropdown" className="bottom-nav-dropdown mr-md-4 ml-md-4 px-1">
-            <NavDropdown.Item href="#fruits">Fruits</NavDropdown.Item>
-            <NavDropdown.Item href="#vagetables">Vegetables</NavDropdown.Item>
-            <NavDropdown.Item href="#electronics">Electronics</NavDropdown.Item>
-            <NavDropdown.Item href="#furniture">Furniture</NavDropdown.Item>
-            <NavDropdown.Item href="#grocery">Grocery</NavDropdown.Item>
-            <NavDropdown.Item href="#meat">Meat</NavDropdown.Item>
+            <NavDropdown.Item>
+              <NavLink to={'/home/All'} className='p-0 text-decoration-none'>All</NavLink>
+            </NavDropdown.Item>
+            <NavDropdown.Item>
+              <NavLink to={'/home/Grocery'} className='p-0 text-decoration-none'>Grocery</NavLink>
+            </NavDropdown.Item>
+            <NavDropdown.Item>
+              <NavLink to={'/home/Pharmacy'} className='p-0 text-decoration-none'>Pharmacy</NavLink>
+            </NavDropdown.Item>
+            <NavDropdown.Item>
+              <NavLink to={'/home/Food'} className='p-0 text-decoration-none'>Food</NavLink>
+            </NavDropdown.Item>
+            <NavDropdown.Item>
+              <NavLink to={'/home/Electronic'} className='p-0 text-decoration-none'>Electronic</NavLink>
+            </NavDropdown.Item>
           </NavDropdown>
           <Navbar.Toggle aria-controls="basic-navbar-nav"/>
           <Navbar.Collapse id="basic-navbar-nav" className="bottom-nav-collapse">
