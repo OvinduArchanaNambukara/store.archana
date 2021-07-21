d9bce8c18746d27e2759b85de250380aafeab6d7
ovindu <ovindunambukara@gmail.com>
2021-07-21 23:26:33 +0530

    fix css transaction key

--- src/components/Routes/Routes.tsx
+++ src/components/Routes/Routes.tsx
@@ -1,5 +1,5 @@
-import React, {lazy, Suspense} from "react";
-import {Route, Switch, useLocation, Redirect} from "react-router-dom";
+import React, {lazy, Suspense, useEffect, useState} from "react";
+import {Redirect, Route, Switch, useLocation} from "react-router-dom";
 import {CSSTransition, TransitionGroup} from "react-transition-group";
 import Loading from "../loading/Loading";
 
@@ -12,10 +12,27 @@ const ProductRoutes = lazy(() => import("../Routes/ProductRoutes"));
 
 const Routes: React.FC = () => {
   let location = useLocation();
+  const [isHomeLoaded, setIsHomeLoaded] = useState<boolean>(true);
+  const [key, setKey] = useState<string>(location.pathname);
+
+  /**
+   * change css transaction key according to route path
+   */
+  useEffect(() => {
+    if (location.pathname.includes('/home') || location.pathname === '/') {
+      if (isHomeLoaded) {
+        setIsHomeLoaded(false);
+        setKey(location.pathname);
+      }
+    } else {
+      setIsHomeLoaded(true);
+      setKey(location.pathname);
+    }
+  }, [location]);
 
   return (
       <TransitionGroup>
-        <CSSTransition classNames='page' timeout={1000} key={location.pathname}>
+        <CSSTransition classNames='page' timeout={1000} key={key}>
           <Suspense fallback={<Loading/>}>
             <Switch location={location}>
               <Route path='/login'>
