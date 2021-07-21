274deb640a0673a83a0c454093c4a3c7f8b36f16
ovindu <ovindunambukara@gmail.com>
2021-07-21 22:09:53 +0530

    fix bottom nav bar category route links

--- src/components/bottom-nav/BottomNav.tsx
+++ src/components/bottom-nav/BottomNav.tsx
@@ -1,26 +1,28 @@
 import React from "react";
 import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
-import {NavLink} from "react-router-dom"
+import {NavLink, useHistory} from "react-router-dom"
 
 const BottomNav: React.FC = () => {
+  const history = useHistory();
+
   return (
       <Container fluid={true}>
         <Navbar className="bottom-nav-bar px-0 py-3 border-bottom" expand="sm">
           <NavDropdown title="Categories" id="basic-nav-dropdown" className="bottom-nav-dropdown mr-md-4 ml-md-4 px-1">
-            <NavDropdown.Item>
-              <NavLink to={'/home/All'} className='p-0 text-decoration-none'>All</NavLink>
+            <NavDropdown.Item onClick={() => history.push('/home/All')}>
+              All
             </NavDropdown.Item>
-            <NavDropdown.Item>
-              <NavLink to={'/home/Grocery'} className='p-0 text-decoration-none'>Grocery</NavLink>
+            <NavDropdown.Item onClick={() => history.push('/home/Grocery')}>
+              Grocery
             </NavDropdown.Item>
-            <NavDropdown.Item>
-              <NavLink to={'/home/Pharmacy'} className='p-0 text-decoration-none'>Pharmacy</NavLink>
+            <NavDropdown.Item onClick={() => history.push('/home/Pharmacy')}>
+              Pharmacy
             </NavDropdown.Item>
-            <NavDropdown.Item>
-              <NavLink to={'/home/Food'} className='p-0 text-decoration-none'>Food</NavLink>
+            <NavDropdown.Item onClick={() => history.push('/home/Food')}>
+              Food
             </NavDropdown.Item>
-            <NavDropdown.Item>
-              <NavLink to={'/home/Electronic'} className='p-0 text-decoration-none'>Electronic</NavLink>
+            <NavDropdown.Item onClick={() => history.push('/home/Electronic')}>
+              Electronic
             </NavDropdown.Item>
           </NavDropdown>
           <Navbar.Toggle aria-controls="basic-navbar-nav"/>
