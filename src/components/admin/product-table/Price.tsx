c4af615a3725acf2045aecdaae83934b5270c949
ovindu <ovindunambukara@gmail.com>
2021-08-23 01:57:27 +0530

    create tableList add to redux store

--- src/components/admin/product-table/Price.tsx
+++ src/components/admin/product-table/Price.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import NumberFormat from "react-number-format";
 
-const Price: React.FC = () => {
+type PriceProps = {
+  price: number | null
+}
+
+const Price: React.FC<PriceProps> = (props) => {
+  const {price} = props;
+
   return (
-      <NumberFormat prefix={'Rs.'} displayType={"text"} value={200000} thousandSeparator={true} suffix={'.00'}/>
+      <NumberFormat prefix={'Rs.'} displayType={"text"} value={price} thousandSeparator={true} suffix={'.00'}/>
   );
 }
 
