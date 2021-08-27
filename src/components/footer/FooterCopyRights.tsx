9bedcaf84874b620d3c87fe3322df5d178135caf
ovindu <ovindunambukara@gmail.com>
2021-08-28 01:15:18 +0530

    change footer and read me file

--- src/components/footer/FooterCopyRights.tsx
+++ src/components/footer/FooterCopyRights.tsx
@@ -5,7 +5,7 @@ const FooterCopyRights: React.FC = () => {
   return (
       <Col xs={12} className='footer-copy-rights text-center py-2'>
             <span>Copyright © 2021. All Rights Reserved.<span
-                className='copy-right'> Powered by SoftVessel</span></span>
+                className='copy-right'> Powered by Austi🦔</span></span>
       </Col>
   );
 }
